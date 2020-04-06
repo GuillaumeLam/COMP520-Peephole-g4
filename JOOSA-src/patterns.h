a1c61fab7833531077d15199a089e8e85d3df391
Adam Gobran <adamgobes@gmail.com>
2020-04-06 14:40:45 -0400

    add simplify_istore pattern

--- JOOSA-src/patterns.h
+++ JOOSA-src/patterns.h
@@ -20,18 +20,21 @@
  */
 
 int simplify_multiplication_right(CODE **c)
-{ int x,k;
-  if (is_iload(*c,&x) && 
-      is_ldc_int(next(*c),&k) && 
-      is_imul(next(next(*c)))) {
-     if (k==0) return replace(c,3,makeCODEldc_int(0,NULL));
-     else if (k==1) return replace(c,3,makeCODEiload(x,NULL));
-     else if (k==2) return replace(c,3,makeCODEiload(x,
-                                       makeCODEdup(
-                                       makeCODEiadd(NULL))));
-     return 0;
-  }
-  return 0;
+{
+	int x, k;
+	if (is_iload(*c, &x) &&
+		is_ldc_int(next(*c), &k) &&
+		is_imul(next(next(*c))))
+	{
+		if (k == 0)
+			return replace(c, 3, makeCODEldc_int(0, NULL));
+		else if (k == 1)
+			return replace(c, 3, makeCODEiload(x, NULL));
+		else if (k == 2)
+			return replace(c, 3, makeCODEiload(x, makeCODEdup(makeCODEiadd(NULL))));
+		return 0;
+	}
+	return 0;
 }
 
 /* dup
@@ -41,13 +44,33 @@ int simplify_multiplication_right(CODE **c)
  * astore x
  */
 int simplify_astore(CODE **c)
-{ int x;
-  if (is_dup(*c) &&
-      is_astore(next(*c),&x) &&
-      is_pop(next(next(*c)))) {
-     return replace(c,3,makeCODEastore(x,NULL));
-  }
-  return 0;
+{
+	int x;
+	if (is_dup(*c) &&
+		is_astore(next(*c), &x) &&
+		is_pop(next(next(*c))))
+	{
+		return replace(c, 3, makeCODEastore(x, NULL));
+	}
+	return 0;
+}
+
+/* dup
+ * istore x
+ * pop
+ * -------->
+ * istore x
+ */
+int simplify_istore(CODE **c)
+{
+	int x;
+	if (is_dup(*c) &&
+		is_istore(next(*c), &x) &&
+		is_pop(next(next(*c))))
+	{
+		return replace(c, 3, makeCODEistore(x, NULL));
+	}
+	return 0;
 }
 
 /* iload x
@@ -56,17 +79,19 @@ int simplify_astore(CODE **c)
  * istore x
  * --------->
  * iinc x k
- */ 
+ */
 int positive_increment(CODE **c)
-{ int x,y,k;
-  if (is_iload(*c,&x) &&
-      is_ldc_int(next(*c),&k) &&
-      is_iadd(next(next(*c))) &&
-      is_istore(next(next(next(*c))),&y) &&
-      x==y && 0<=k && k<=127) {
-     return replace(c,4,makeCODEiinc(x,k,NULL));
-  }
-  return 0;
+{
+	int x, y, k;
+	if (is_iload(*c, &x) &&
+		is_ldc_int(next(*c), &k) &&
+		is_iadd(next(next(*c))) &&
+		is_istore(next(next(next(*c))), &y) &&
+		x == y && 0 <= k && k <= 127)
+	{
+		return replace(c, 4, makeCODEiinc(x, k, NULL));
+	}
+	return 0;
 }
 
 /* goto L1
@@ -84,17 +109,21 @@ int positive_increment(CODE **c)
  * L2:    (reference count increased by 1)  
  */
 int simplify_goto_goto(CODE **c)
-{ int l1,l2;
-  if (is_goto(*c,&l1) && is_goto(next(destination(l1)),&l2) && l1>l2) {
-     droplabel(l1);
-     copylabel(l2);
-     return replace(c,1,makeCODEgoto(l2,NULL));
-  }
-  return 0;
+{
+	int l1, l2;
+	if (is_goto(*c, &l1) && is_goto(next(destination(l1)), &l2) && l1 > l2)
+	{
+		droplabel(l1);
+		copylabel(l2);
+		return replace(c, 1, makeCODEgoto(l2, NULL));
+	}
+	return 0;
 }
-void init_patterns(void) {
+void init_patterns(void)
+{
 	ADD_PATTERN(simplify_multiplication_right);
 	ADD_PATTERN(simplify_astore);
+	ADD_PATTERN(simplify_istore);
 	ADD_PATTERN(positive_increment);
 	ADD_PATTERN(simplify_goto_goto);
 }
